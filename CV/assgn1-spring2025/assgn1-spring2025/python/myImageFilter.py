a6413a53e739a2ae7fc1bf11985a0fee1bc1fb6c
Belle Connaught <belleconnaught@belles-mbp.wifi.local.cmu.edu>
2025-01-24 19:28:51 -0500

    barely did anything ngl

--- CV/assgn1-spring2025/assgn1-spring2025/python/myImageFilter.py
+++ CV/assgn1-spring2025/assgn1-spring2025/python/myImageFilter.py
@@ -1,4 +1,9 @@
 import numpy as np
 
+'''
+@param img0: grayscale image
+@param h:    matrix storing  a convolution filter
+'''
 def myImageFilter(img0, h):
-    # YOUR CODE HERE
+    w, h = np.sha
+    img1 = np.zeros((h, w))
