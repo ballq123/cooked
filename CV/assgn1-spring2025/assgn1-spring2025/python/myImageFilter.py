e1ebcb9893fbf0847c95005f521fcf1ce1466759
ballq123 <connaughtb@gmail.com>
2025-01-24 20:14:13 -0500

    wrote image filter

--- CV/assgn1-spring2025/assgn1-spring2025/python/myImageFilter.py
+++ CV/assgn1-spring2025/assgn1-spring2025/python/myImageFilter.py
@@ -1,9 +1,20 @@
 import numpy as np
+import cv2
 
 '''
-@param img0: grayscale image
+@param img0: grayscale (2D) image, ** assuming it's a np array **
 @param h:    matrix storing  a convolution filter
 '''
 def myImageFilter(img0, h):
-    w, h = np.sha
-    img1 = np.zeros((h, w))
+    imageH, imageW = np.shape(img0)
+    fH, fW = np.shape(h)
+    paddingH, paddingW = fH // 2, fW // 2
+
+    padded = np.pad(img0, ((paddingH, paddingH), (paddingW, paddingW)), mode='edge')
+    img1 = np.zeros_like(img0)
+
+    for i in range(imageH):
+        for j in range(imageW):
+            subsection = padded[i: i + fH, j: j + fW]
+            img1[i, j] = np.sum(subsection * h)
+    return img1
