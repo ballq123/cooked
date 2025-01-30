0aee62020d923fd14340d1f9ce113da9425384bb
ballq123 <connaughtb@gmail.com>
2025-01-30 11:48:34 -0500

    edge detection works but the nmaxsuppression lowkey sucks, lines are too thick

--- CV/assgn1-spring2025/assgn1-spring2025/python/myEdgeFilter.py
+++ CV/assgn1-spring2025/assgn1-spring2025/python/myEdgeFilter.py
@@ -14,29 +14,29 @@ from myImageFilter import myImageFilter
 '''
 def myEdgeFilter(img0, sigma):
     hsize = 2 * math.ceil(3 * sigma) + 1
-    # have to do some weird stuff with this kernel's shape ngl
+    # mighttt have to do some weird stuff with this kernel's shape ngl
     kernel = signal.windows.gaussian(hsize, sigma)
     kernel = np.outer(kernel, kernel)
-    # normalize dat bihh
+    # normalize dat bihh (TAKE THIS OUT LATER)
     kernel = (1 / np.sum(kernel)) * kernel
 
     # print("kernel shape after: ", kernel.shape)
     smoothed = myImageFilter(img0, kernel)
-
     xOrientedSobel = np.array([[1., 0., -1.]])
-    imgX = myImageFilter(smoothed, xOrientedSobel)
     yOrientedSobel = np.array([[1.],
                                [0.],
                                [-1.]])
+    imgX = myImageFilter(smoothed, xOrientedSobel)
     imgY = myImageFilter(smoothed, yOrientedSobel)
 
-    gradMag = np.sqrt((imgX ** 2) + (imgY ** 2))
-    # returns angles in range [-pi, pi) = [-180, 180)
+    # angles below are in range [-pi, pi) = [-180, 180)
     gradAngleRadians = np.arctan2(imgY, imgX)
     gradAngle = ((gradAngleRadians * 180) / np.pi) % 180
+    gradMag = np.sqrt((imgX ** 2) + (imgY ** 2))
 
     img1 = nMaxSuppression(smoothed, gradMag, gradAngle)
     return img1, imgX, imgY
+    
 
 '''
 @param gradMag:     2D numpy array
@@ -45,78 +45,64 @@ def myEdgeFilter(img0, sigma):
                     Gradient angle matrix of image
 '''
 def nMaxSuppression(smoothed, gradMag, gradAngle):
-    gradH, gradW = gradMag.shape
-    shaved = np.zeros_like(gradMag)
+    rows, cols = smoothed.shape
+    shaved = np.zeros_like(smoothed)
 
     zeroDeg = np.array([
-        [[0, 0, 0],
-         [1, 1, 1],
-         [0, 0, 0]]
-    ])
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ], dtype=np.uint8)
     fourFiveDeg = np.array([
-        [[0, 0, 1],
-         [0, 1, 0],
-         [1, 0, 0]],
-    ])
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ], dtype=np.uint8)
     ninetyDeg = np.array([
-        [[0, 1, 0],
-         [0, 1, 0],
-         [0, 1, 0]],
-    ])
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ], dtype=np.uint8)
     oneThreeFiveDeg = np.array([
-        [[1, 0, 0],
-         [0, 1, 0],
-         [0, 0, 1]],
-    ])
+        [0, 0, 1],
+        [0, 1, 0],
+        [1, 0, 0]
+    ], dtype=np.uint8)
 
-    dilateZero = dilate(smoothed, zeroDeg)
-    dilateFour = dilate(smoothed, fourFiveDeg)
-    dilateNine = dilate(smoothed, ninetyDeg)
-    dilateOne = dilate(smoothed, oneThreeFiveDeg)
+    gradMag = gradMag.astype(np.float32)  # Ensure float32 for dilation
+    gradMag = np.expand_dims(gradMag, axis=-1)
+
+    print("gradMag shape after conversion:", gradMag.shape)
+    print("Kernel shape:", zeroDeg.shape)
 
 
+    # mod 4 wraps 180 degrees back to 0
+    # use .astype(int) because using int() on an array throws errors :(
+    nearestAngle = np.round(gradAngle / float(45))
+    index = (nearestAngle.astype(int)) % 4
 
+    dilateZero = dilate(gradMag, zeroDeg)
+    dilateFour = dilate(gradMag, fourFiveDeg)
+    dilateNine = dilate(gradMag, ninetyDeg)
+    dilateOne = dilate(gradMag, oneThreeFiveDeg)
+    dilations = [dilateZero, dilateFour, dilateNine, dilateOne]
 
-    '''
-
-    # skip bordering pixels bc they don't have all necessary neighbors 😞  
-    # DO THIS, OR SHOULD WE PAD THE IMAGE & ALLOW FOR BORDER INCLUSION?  
-    for i in range(1, gradH - 1):
-        for j in range(1, gradW - 1):
-            angle = gradAngle[i, j]
-            # horizontal (left -> right, or vice-versa)
-            if ((angle >= 0 and angle < 22.5) or (angle >= 157.5 and angle <= 180)):
-                left = gradMag[i, j-1]
-                right = gradMag[i, j+1]
-            # diagonal (bottom-left -> top-right, or vice-versa)
-            elif (angle >= 22.5 and angle < 67.5):
-                left = gradMag[i+1, j-1]
-                right = gradMag[i-1, j+1]
-            # vertical (top -> bottom, or vice-versa)
-            elif (angle >= 67.5 and angle < 112.5):
-                left = gradMag[i-1, j]
-                right = gradMag[i+1, j]
-            # diagonal (top-left -> bottom-right, or vice-versa)
+    for row in range(rows):
+        for col in range(cols):
+            # select corresponding dilation based on angle idx
+            current_dilation = dilations[index[row, col]]
+            if gradMag[row, col] == current_dilation[row, col]:
+                shaved[row, col] = gradMag[row, col]
             else:
-                # angle >= 112.5 and angle < 157.5
-                left = gradMag[i-1, j-1]
-                right = gradMag[i+1, j+1]
-
-            # "value of a center pixel is set to 0 if its gradient magnitude is 
-            #   less than either one or both of its neighbors' values"
-            curMag = gradMag[i, j]
-            if (curMag > left) and (curMag > right):
-                shaved[i, j] = curMag
-            else:
-                shaved[i, j] = 0
-    '''
+                shaved[row, col] = 0
     return shaved
+
 
 def showRes():
     sigma = 2
     img = cv2.imread('img01.jpg', cv2.IMREAD_GRAYSCALE)
     assert img is not None, "file DNE? we can't read it"
-    edges_custom = myEdgeFilter(img, sigma)[2]
+    edges_custom, _, _ = myEdgeFilter(img, sigma)
     plt.imshow(edges_custom, cmap='gray')
     plt.title('My Edge Filter')
     plt.axis('off')
